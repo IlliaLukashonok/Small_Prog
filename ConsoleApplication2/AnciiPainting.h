1ba70e2c7cee22fcc35ae4f8916f36809a0202b5
Илья Лукашонок <l.ilya@outlook.com>
2019-11-26 21:45:29 +0300

    Little logo disign fix

--- ConsoleApplication2/AnciiPainting.h
+++ ConsoleApplication2/AnciiPainting.h
@@ -17,8 +17,8 @@ void MyLogo(const char* date)
 	printf("            /   /          /   /   /  _____/ \n");
 	printf("           /   /          /   /   |  (___ \n");
 	printf("          /   /          /   /     \\___  \\ \n");
-	printf("         /   /______    /   /     _____)  | \n");
-	printf("        /__________/   /___/     /_______/ \n\n");
+	printf("         /   /______    /   /    ______)  | \n");
+	printf("        /__________/   /___/    /________/ \n\n");
 	printf("%s Miensk, Bielarus\n", date);
 	AnciiPaint(75, "=");
 }
